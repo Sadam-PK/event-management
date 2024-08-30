1caed3db49bd96a0873cc196860f93b5510f75d4
sadam-PK <sadamccr@gmail.com>
2024-08-31 02:47:46 +0500

    chat and message models added

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 // Define mongoose schemas
 
+// user schema
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
@@ -8,11 +9,14 @@ const userSchema = new mongoose.Schema({
   events: [{ type: mongoose.Schema.Types.ObjectId, ref: "Event" }], // Events they created or joined
 });
 
+// admin schema
+
 const adminSchema = new mongoose.Schema({
   username: String,
   password: String,
 });
 
+// event schema
 const eventSchema = new mongoose.Schema({
   title: { type: String, required: true },
   createdBy: {
@@ -23,12 +27,36 @@ const eventSchema = new mongoose.Schema({
   attendees: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }], // References to Attendees
 });
 
+// message schema
+const messageSchema = new mongoose.Schema({
+  organizer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  attendee: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  content: { type: String, required: true },
+});
+
+// chat schema
+const chatSchema = new mongoose.Schema({
+  messages: [{ type: mongoose.Schema.Types.ObjectId, ref: "Message" }],
+});
+
 const User = mongoose.model("User", userSchema);
 const Admin = mongoose.model("Admin", adminSchema);
 const Event = mongoose.model("Event", eventSchema);
+const Message = mongoose.model("Message", messageSchema);
+const Chat = mongoose.model("Chat", chatSchema);
 
 module.exports = {
   User,
   Admin,
   Event,
+  Message,
+  Chat,
 };
